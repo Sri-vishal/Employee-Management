40efcee69109eb92aedbfe5ae938561b05f7ca20
Srivishal <166016807+Sri-vishal@users.noreply.github.com>
2024-06-24 12:33:00 +0530

    Add files via upload

---
+++ Thymeleaf/src/main/java/com/Thymeleaf/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.Thymeleaf.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.Thymeleaf.Entity.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
